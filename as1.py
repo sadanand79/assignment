182beb70cce00ba8656d26fa6fe8dc1110265125
sadanand79 <67182264+sadanand79@users.noreply.github.com>
2020-07-10 23:20:14 +0530

    Add files via upload

---
+++ as1.py
@@ -0,0 +1,43 @@
+"""1.Simple Message: Store a message in a variable, and then print that message.
+Name='S Dombale'
+print("Name:",Name)
+2.Store a message in a variable and print that message. Then change the value of your variable to a new message andprint the new message.
+Name='S Dombale'
+print("Name:",Name)
+Name='sadanand'
+print("Name:",Name)
+
+
+Name= "SD"
+print ("Hello %s, would you like to learn some Python today?"%(Name))
+
+quote = 'John Locke once said,“I have always thought the actions of men the best interpreters of their thoughts.”'
+print (quote)
+
+
+print('{0}\n{1}\n{2}\n{3}\n'.format(5+3,9-1,2*4,int(16/2)))
+
+my_fav=25
+print("My favourite number is:",my_fav)
+
+#10-july-2020 SADANAND
+print('{0}\n{1}\n{2}\n{3}\n'.format(5+3,9-1,2*4,int(16/2))) #simple airthmatic operations
+
+quote = 'John Locke once said,“I have always thought the actions of men the best interpreters of their thoughts.”'
+print (quote) #Print a simple quote 
+
+
+Names=["praveen","Bassu","Manjunath","Swami"]
+for friends in Names:
+    #print("hello guys {0}" .format(friends))
+    print(friends) 
+
+
+Names=["praveen","Bassu","Manjunath","Swami"]
+for friends in Names:
+    print("How are you {0}" .format(friends))
+    
+fav_mode  = 'Car'
+stores = ["Abarth","Acura","Alfa Romeo","Aston Martin","Audi","Bentley","BMW"]
+for Cars in stores:
+  print (" I would like to drive a {0} {1}".format(Cars,fav_mode))"""
